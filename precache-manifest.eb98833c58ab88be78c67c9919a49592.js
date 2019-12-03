1375a50fd98fe21717ceed8e71d393a3d9225fc6
awbridgers <awbridgers@users.noreply.github.com>
2019-12-03 10:45:54 -0500

    Updates

---
+++ precache-manifest.eb98833c58ab88be78c67c9919a49592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8eda1509bc1ed0905c4b8eeb64e3b82",
+    "url": "/football-analysis/index.html"
+  },
+  {
+    "revision": "244a6d3ac229bb4066a4",
+    "url": "/football-analysis/static/css/main.2a40c941.chunk.css"
+  },
+  {
+    "revision": "fcc63cc2da8b994d65b9",
+    "url": "/football-analysis/static/js/2.217ba974.chunk.js"
+  },
+  {
+    "revision": "244a6d3ac229bb4066a4",
+    "url": "/football-analysis/static/js/main.83269f84.chunk.js"
+  },
+  {
+    "revision": "bbcca1738fa2906d267f",
+    "url": "/football-analysis/static/js/runtime-main.821890ed.js"
+  }
+]);
